7735c92a1a20f26e7241cd857f3391d41c58ac13
Yusuf2668 <75602023+Yusuf2668@users.noreply.github.com>
2021-01-21 17:04:21 +0300

    Add files via upload

---
+++ MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    public class MyDictionary<Key, Value>
+    {
+        private Key[] keys;
+        private Value[] vlaues;
+        public MyDictionary()
+        {
+            keys = new Key[0];
+            vlaues = new Value[0];
+        }
+        public void Add(Key Keys, Value values)
+        {
+            Key[] Tkey = keys;
+            Value[] Tvalues = vlaues;
+            keys = new Key[keys.Length + 1];
+            vlaues = new Value[vlaues.Length + 1];
+            for (int i = 0; i < keys.Length-1; i++)
+            {
+                keys[i] = Tkey[i];
+                vlaues[i] = Tvalues[i];
+            }
+            vlaues[vlaues.Length - 1] = values;
+            keys[keys.Length - 1] = Keys;
+        }
+
+        public int Count()
+        {
+            return keys.Length;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> lisDictionary = new MyDictionary<string, int>();
+            lisDictionary.Add("yusuf",2);
+            
+        }
+    }
+}
